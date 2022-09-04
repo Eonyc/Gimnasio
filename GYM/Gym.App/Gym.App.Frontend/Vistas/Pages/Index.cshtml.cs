388041dabdf14a40ef4fb5532530342dcf621e81
Alexander Pardo <alexanderpardo@outlook.com>
2022-09-04 01:20:52 -0500

    Se crea nueva página y categorías en el encabezado

---
+++ GYM/Gym.App/Gym.App.Frontend/Vistas/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+ // código para insertar páginas
+ //  dotnet new page -n ProgramasEjercicio -na Vistas.Pages -o .\
+namespace Vistas.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
