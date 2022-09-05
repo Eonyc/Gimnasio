2f98bb65a801d270377fb37aad975b4ba5bb1150
Alexander Pardo <alexanderpardo@outlook.com>
2022-09-04 19:01:02 -0500

    Aplicación funcionando bien y con páginas home, Programas, Nutrition, customer y about es su estádo más Basico

--- GYM/Gym.App/Gym.App.Frontend/Vistas/wwwroot/js/site.js
+++ GYM/Gym.App/Gym.App.Frontend/Vistas/wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+//Javascript Jesus para formulario
+var array = document.getElementsByClassName('formularioInput');
+for (var i = 0; i < array.length; i++){
+    array[i].addEventListener('keyup', function(){
+        if (this.value.length >= 1){
+            this.nextElementSibling.classList.add('fijar');
+        } else{
+            this.nextElementSibling.classList.remove('fijar');
+        }
+    }); 
+}
